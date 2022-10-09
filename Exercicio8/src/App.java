340129f04a14677af147b4f9975f45dd9f3e71df
rminoro <rminoro@outlook.com>
2022-10-09 17:22:16 -0300

    exercicio 8 finalizado

--- Exercicio8/src/App.java
+++ Exercicio8/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //quantas horas trabalha e quanto ganha
+        Scanner S = new Scanner (System.in);
+        
+        System.out.println("Insira valor/hora");
+        float valor_hora= S.nextFloat();
+        System.out.println("Quantidade de horas trabalhadas por mês");
+        float horas_mes = S.nextFloat();
+        //Calcular e mostrar o salario do mês
+        float salario_mes = valor_hora*horas_mes;
+
+        System.out.println("O salário do mês é:"+ salario_mes);
     }
 }
